4c3ea56baa8708323ad54ed9b1ce087be084abd6
wadayuki <106656539+lu3-yk@users.noreply.github.com>
2022-09-15 10:59:18 +0000

    unless文・case文・三項演算子について勉強

--- cp_2/2-10.rb
+++ cp_2/2-10.rb
@@ -5,7 +5,7 @@ puts 1 && false && 3
 
 puts nil || false
 puts || nil
-nil || false || 2 || 3
+puts nil || false || 2 || 3
 
 #unless文
 status = "error"
@@ -19,3 +19,44 @@ status = "ok"
   else
     puts "正常です"
   end
+
+#case文
+# 一つのオブジェクトや式を複数の値と比較する場合は、elsifを重ねるよりもcase文で書いた方がシンプルになります。
+country = "italy"
+#if文を使う場合
+if country == "japan"
+  puts "こんにちは"
+elsif country == "us"
+  puts "Hello"
+elsif country == "italy"
+  puts "Ciao"
+else
+  puts "???"
+end
+
+#case文を使う場合
+case country
+when "japan"
+  puts "こんにちは"
+when "us"
+  puts "Hello"
+when "italy"
+  puts "Ciao"
+else
+  puts "???"
+end
+
+#条件演算子（三項演算子）
+#式？真だった場合の処理：偽だった場合の処理
+
+#if文だと
+n = 11
+if n > 10
+  puts "10より大きい"
+else
+  puts "10以下"
+end
+
+#このコードを三項演算子を使うことで
+n = 11
+ puts n > 10 ? "10より大きい" : "10以下"

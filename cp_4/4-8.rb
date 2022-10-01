c9c133e5d84bc9639a33dfd2f0803d36f0160fd2
wadayuki <106656539+lu3-yk@users.noreply.github.com>
2022-10-01 11:29:08 +0000

    each_with_indexについて

---
+++ cp_4/4-8.rb
@@ -0,0 +1,16 @@
+# ブロックについてもっと詳しく
+# 添え字つきの繰り返し処理
+# Rubyではeachメソッドを使うのが繰り返し処理の基本。しかし。eachメソッドでは何番目の要素を繰り返しているのか、ブロック内で判別できない。
+# 繰り返し処理をしつつ、処理している要素もよえじも取得したい。そんな時はeach_with_indexを使う。
+# each_with_indexを使うと、ブロックパラメーターの第二パラメータに添え字を渡してくれる。
+fruits = ['apple','orange','melon']
+# ブロックパラメータのiには0,1,3...と要素の添え字が入っている
+fruits.each_with_index{ |fruit,i| puts '#{i}: #{fruit}' }
+
+# with_indexメソッドを使った添え字つきの繰り返し処理
+# each_with_indexメソッドだと、eachメソッドの代わりにしか使えない。
+# 例えばmapメソッドで繰り返し処理をしつつ、添字も同時に取得したいときはmapメソッドとwith_indexメソッドを組み合わせて使う。
+
+fruits = ['apple','orange','melon']
+# mapとして処理しつつ、添字も受け取る
+fruits.map.each_with_index{ |fruit,i| puts '#{i}: #{fruit}' }

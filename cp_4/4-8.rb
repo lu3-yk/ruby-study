f23502b0a8ee0d97192c714f56f88b6b17a47807
wadayuki <106656539+lu3-yk@users.noreply.github.com>
2022-10-02 14:42:03 +0000

    with_indexメソッドの使い方

--- cp_4/4-8.rb
+++ cp_4/4-8.rb
@@ -5,12 +5,15 @@
 # each_with_indexを使うと、ブロックパラメーターの第二パラメータに添え字を渡してくれる。
 fruits = ['apple','orange','melon']
 # ブロックパラメータのiには0,1,3...と要素の添え字が入っている
-fruits.each_with_index{ |fruit,i| puts '#{i}: #{fruit}' }
+fruits.each_with_index{ |fruit, i| p '#{i}: #{fruit}' }
+
+[5, 10, 15].each.with_index(1) do |n, i|
+  p [n, i]
+end
 
 # with_indexメソッドを使った添え字つきの繰り返し処理
 # each_with_indexメソッドだと、eachメソッドの代わりにしか使えない。
 # 例えばmapメソッドで繰り返し処理をしつつ、添字も同時に取得したいときはmapメソッドとwith_indexメソッドを組み合わせて使う。
-
-fruits = ['apple','orange','melon']
 # mapとして処理しつつ、添字も受け取る
-fruits.map.each_with_index{ |fruit,i| puts '#{i}: #{fruit}' }
+fruits = ['apple','orange','melon']
+puts fruits.map.each_with_index{ |fruit, i| puts '#{i}: #{fruit}' }

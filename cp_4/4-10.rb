c82c9061a397c067b0d01b8e9900158f897f43f2
wadayuki <106656539+lu3-yk@users.noreply.github.com>
2022-10-07 14:50:51 +0000

    繰り返し処理について

--- cp_4/4-10.rb
+++ cp_4/4-10.rb
@@ -35,4 +35,47 @@ puts a
 # その一つがwhile文。while文は指定した条件が真の間、処理を繰り返す。
 # while 条件式
 #   繰り返したい処理
+# end
+# 例えば以下は配列の要素数が5つになるまで値を追加するwhile文
+a = []
+while a.size < 5
+  a << 1
+end
+puts a
+# 条件式の後ろにdoを入れると１行で書くこともできます。
+a = []
+while a.size < 5 do a << 1 end
+puts a
+
+# しかし、１行で書くのであれば修飾子としてwhile文を後ろに置いた方がスッキリかける
+a = []
+a << 1 while a.size < 5
+puts a
+
+# どんな条件式でも最低１回は実行したいという場合はbegin...endで囲んでからwhileをかく
+a = []
+while false
+  # このコードは常に条件が気になるので実行されない
+  a << 1
+end
+
+puts a
+
+# begin ... endで囲むとどんな条件式でも最低１回は実行される
+  begin
+    a << 2
+  end
+  puts a
+  
+# for文
+# 配列やハッシュはfor文で繰り返し処理することもできる
+# for 変数 in 配列やハッシュ
+#   繰り返し処理
 # end  
+# 例えば次は配列の中身を順番に加算していくコード
+numbers = [1,2,3,4]
+sum = 0
+for n in numbers
+  sum += n
+end
+puts sum
